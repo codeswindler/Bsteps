793b878f92ac76bad25acb184ba06823969417c7
Wilson Muiru <36936748+codeswindler@users.noreply.github.com>
2022-09-27 00:43:52 +0300

    Add files via upload

---
+++ feedback/about.php
@@ -0,0 +1,7 @@
+<?php include 'inc/header.php'; ?>
+    <h2>About</h2>
+    <br>
+    <br>
+
+    <p class="text-center">AdvantaSMS is the top bulk SMS marketing company offering effective bulk SMS marketing solutions <br> to different business verticals and companies across the globe.</p>
+    <?php include 'inc/footer.php'; ?>
